4d774d13095e75769b7487aad301a7e16ea6ea26
ANIEKWECHIDERA <c.hideraaniekwederah73@gmail.com>
2025-06-04 21:55:56 +0100

    Refactor App component structure for improved readability

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,26 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="flex flex-col space-y-7 justify-center items-center h-screen bg-gray-100">
+    <>
       <Header />
-      <Button
-        text={"Get Started Button"}
-        classname={"hover:bg-[#1E40AF] bg-[#2563EB] w-52"}
-      />
-      <Button2
-        text={"Sign up"}
-        classname={"hover:bg-[#1E40AF] w-32 bg-[#2563EB] "}
-      />
-      <TestimonialCard
-        name="Jane Doe"
-        position="Software Engineer"
-        testimonial="This platform has greatly improved my productivity!"
-        img="https://via.placeholder.com/150"
-      />
+      <div className="flex flex-col space-y-7 justify-center items-center h-screen bg-gray-100">
+        <Button
+          text={"Get Started Button"}
+          classname={"hover:bg-[#1E40AF] bg-[#2563EB] w-52"}
+        />
+        <Button2
+          text={"Sign up"}
+          classname={"hover:bg-[#1E40AF] w-32 bg-[#2563EB] "}
+        />
+        <TestimonialCard
+          name="Jane Doe"
+          position="Software Engineer"
+          testimonial="This platform has greatly improved my productivity!"
+          img="https://via.placeholder.com/150"
+        />
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
